26f99f88eb8a81ead3920194db84f39293ba8982
Ilkin Aliyev <55974939+ilkin-aliyev@users.noreply.github.com>
2021-10-06 11:03:59 +0400

    Update ValidationExceptionCode.java

--- src/main/java/spring/crud/jdbc/model/exception/ValidationExceptionCode.java
+++ src/main/java/spring/crud/jdbc/model/exception/ValidationExceptionCode.java
@@ -4,5 +4,5 @@ public abstract class ValidationExceptionCode {
     public static final String INVALID_MIN_AGE = "INVALID_MIN_AGE";
     public static final String INVALID_MAX_AGE = "INVALID_MAX_AGE";
     public static final String FIELD_CANT_BE_NULL = "FIELD_CANT_BE_NULL";
-    public static final String FIELD_CANT_BE_BLANK = "NAME_CANT_BE_BLANK";
+    public static final String FIELD_CANT_BE_BLANK = "FIELD_CANT_BE_BLANK";
 }
